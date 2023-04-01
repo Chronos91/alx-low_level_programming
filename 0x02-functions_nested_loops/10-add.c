fa7dad6e655e6d515699583711312b5d2692906b
Chronos <victorabuke@gmail.com>
2023-04-01 15:40:55 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - sums up two integers
+  * @x: first int
+  * @y: second int
+  *
+  * Return: Sum
+  */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+void times_table(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; x < 10; y++)
+		{
+			_putchar(y + 

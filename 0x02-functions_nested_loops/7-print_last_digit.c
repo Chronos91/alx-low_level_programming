11559cadb5832c989d09aa2b77e7927cc213fcbd
Chronos <victorabuke@gmail.com>
2023-04-01 15:01:15 -0700

    Prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+  * print_last_digit - prints last digit
+  * @i: value it takes
+  *
+  * Return: last digit
+  */
+
 int print_last_digit(int i)
 {
 	int n;
-	
+
 	n = i % 10;
-	if ( i < 0)
+	if (i < 0)
 		n = -n;
+	_putchar(n + '0');
 	return (n);
 }

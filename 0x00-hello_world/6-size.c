3bf1507dacb4d7fea38605cd058b59994515fb2b
Chronos <victorabuke2@gmail.com>
2023-09-13 04:02:13 -0700

    test2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,12 @@ int main(void)
 	int i;
 	char c;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	
 	return (0);
-}	
+}
